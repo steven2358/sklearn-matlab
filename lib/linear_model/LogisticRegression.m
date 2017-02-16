a5f218a058bd8d30c15699a6d6f56c95b2e8d364
Steven Van Vaerenbergh <steven2358@gmail.com>
2017-02-16 10:17:37 +0100

    add sample weights to LogisticRegression

--- lib/linear_model/LogisticRegression.m
+++ lib/linear_model/LogisticRegression.m
@@ -3,6 +3,7 @@ classdef LogisticRegression < handle
     properties
         reg = 1E-2; % regularization
         coef_; % coefficients
+        sample_weights; % weights for individual samples
     end
     
     methods
@@ -22,11 +23,16 @@ classdef LogisticRegression < handle
             lambda = obj.reg;
             w = (X'*X+lambda*eye(size(X,2)))\X'*Y;
             
+            if isempty(obj.sample_weights)
+                obj.sample_weights = ones(size(X,1),1);
+            end
+            sw = obj.sample_weights;
+            
             % logistic regression
             options = optimset('GradObj', 'on', 'MaxIter', 400,...
                 'Display', 'off');
             [w, J, exit_flag] = ...
-                fminunc(@(t)(log_cost_function_reg(t, X, Y, lambda)),...
+                fminunc(@(t)(log_cost_function_reg(t,X,Y,lambda,sw)),...
                 w, options); %#ok<ASGLU>
             
             obj.coef_ = w(:);
--- lib/linear_model/log_cost_function_reg.m
+++ lib/linear_model/log_cost_function_reg.m
@@ -1,12 +1,17 @@
-function [J, grad] = log_cost_function_reg(theta, X, y, lambda)
+function [J, grad] = log_cost_function_reg(theta, X, y, lambda, weights)
 % LOG_COST_FUNCTION_REG Compute cost and gradient for logistic regression
 % with regularization.
 %   J = LOG_COST_FUNCTION_REG(theta, X, y, lambda) computes the cost of
 %   using theta as the parameter for regularized logistic regression and
 %   the gradient of the cost w.r.t. to the parameters.
+%   w is optional sample weight.
 
 % Initialize some useful values
 m = length(y); % number of training examples
+
+if nargin<5
+    weights = ones(m,1);
+end
 
 % Compute the cost of a particular choice of theta.
 
@@ -16,9 +21,9 @@ theta_reg = theta;
 theta_reg(1) = 0; % exclude theta(1) from regularization
 
 % cost
-J = 1/m*(-y'*log(h) - (1-y')*log(1-h)) + ...
+J = 1/m*(-y'*(log(h).*weights) - (1-y')*((log(1-h)).*weights)) + ...
     lambda/2/m*(theta_reg'*theta_reg);
 
 % gradient
-grad = 1/m*X'*(h-y) + lambda/m*theta_reg;
+grad = 1/m*X'*((h-y).*weights) + lambda/m*theta_reg;
 end
